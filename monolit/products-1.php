de3c6de6fbdac41b3140061048b4bd09250490f3
dizzyeyes <909338300@qq.com>
2016-03-09 16:30:43 +0800

    change home.php about.php products.php
    0. pick out navbar and footer into indivisual files.
    1. change home.php
    2. change about.php
    3. products.php

---
+++ monolit/products-1.php
@@ -0,0 +1,277 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <!--=============== basic  ===============-->
+        <meta charset="UTF-8">
+        <title>北京万宝力防水防腐技术开发有限公司</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta name="robots" content="index, follow"/>
+        <meta name="keywords" content=""/>
+        <meta name="description" content=""/>
+        <!--=============== css  ===============-->	
+        <link type="text/css" rel="stylesheet" href="css/reset.css">
+        <link type="text/css" rel="stylesheet" href="css/plugins.css">
+        <link type="text/css" rel="stylesheet" href="css/style.css">
+        <!--=============== favicons ===============-->
+        <link rel="shortcut icon" href="images/favicon.ico">
+    </head>
+    <body>
+        <!-- loader -->
+        <div class="loader">
+            <div id="movingBallG">
+                <div class="movingBallLineG"></div>
+                <div id="movingBallG_1" class="movingBallG"></div>
+            </div>
+        </div>
+        <!-- loader end -->
+        <!--================= main start ================-->
+        <div id="main">
+            <!--=============== header ===============-->	
+            <header>
+                <!-- header-inner  -->
+                <div class="header-inner">
+                    <!-- header logo -->
+                    <div class="logo-holder">
+                        <a href="index.html"><img src="images/logo.png" alt=""></a>
+                    </div>
+                    <!-- header logo end -->
+                    <!-- mobile nav button -->
+                    <div class="nav-button-holder">
+                        <div class="nav-button vis-m"><span></span><span></span><span></span></div>
+                    </div>
+                    <!-- mobile nav button end -->
+                    
+                    <!-- navigation  -->                    
+                    <?php 
+                        include_once("navbar.html");
+                    ?>  
+                    <!-- navigation  end -->
+                </div>
+                <!-- header-inner  end -->
+                <!-- share button  -->
+                <div class="show-share isShare">
+                    <span>分享</span>
+                    <i class="fa fa-chain-broken"></i>            
+                </div>
+                <!-- share  end-->
+            </header>
+            <!--=============== wrapper ===============-->	
+            <div id="wrapper">
+                <!-- content-holder  -->
+                <div class="content-holder">
+                    <!-- Page title -->
+                    <div class="dynamic-title">产品中心</div>
+                    <!-- Page title  end--> 
+                    <!-- content  -->
+                    <div class="content">
+                        <section class="parallax-section">
+                            <div class="parallax-inner">
+                                <div class="bg" data-bg="images/bg/1.jpg" data-top-bottom="transform: translateY(300px);" data-bottom-top="transform: translateY(-300px);"></div>
+                                <div class="overlay"></div>
+                            </div>
+                            <div class="container">
+                                <div class="page-title">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <h2>改性防水卷材系列</h2>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <ul class="creat-list">
+                                                <li><a href="home.html">网站首页</a></li>
+                                                <li><a href="products.html">产品中心</a></li>
+                                                <li><a href="products-1.html">改性防水卷材系列</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                    <!-- content end -->
+                    <div class="content">
+                        <section>
+                            <div class="container">
+                                <!--=============== portfolio holder ===============-->
+                                <div class="gallery-items   three-columns grid-small-pad lightgallery">
+                                    <!-- 1 -->
+                                    <div class="gallery-item">
+                                        <div class="grid-item-holder">
+                                            <div class="box-item">
+                                                <a data-src="images/folio/products/products-1/1.jpg" class="popup-image" data-sub-html="alteration in some">
+                                                <span class="overlay"></span> 
+                                                <img  src="images/folio/products/products-1/thumbs/1.jpg"   alt="">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- 1 end-->
+                                    <!-- 2 -->
+                                    <div class="gallery-item">
+                                        <div class="grid-item-holder">
+                                            <div class="box-item">
+                                                <a data-src="images/folio/products/products-1/2.jpg" class="popup-image" data-sub-html="majority have">
+                                                <span class="overlay"></span> 
+                                                <img  src="images/folio/products/products-1/thumbs/2.jpg"   alt="">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- 2 end-->
+                                    <!-- 3 -->
+                                    <div class="gallery-item">
+                                        <div class="grid-item-holder">
+                                            <div class="box-item">
+                                                <a data-src="images/folio/products/products-1/3.jpg" class="popup-image" data-sub-html="many variations">
+                                                <span class="overlay"></span> 
+                                                <img  src="images/folio/products/products-1/thumbs/3.jpg"   alt="">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- 3 end-->                          
+                                    <!-- 4 -->
+                                    <div class="gallery-item">
+                                        <div class="grid-item-holder">
+                                            <div class="box-item">
+                                                <a data-src="images/folio/products/products-1/4.jpg" class="popup-image" data-sub-html="making this">
+                                                <span class="overlay"></span> 
+                                                <img  src="images/folio/products/products-1/thumbs/4.jpg"   alt="">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- 4 end-->
+                                    <!-- 5 -->
+                                    <div class="gallery-item">
+                                        <div class="grid-item-holder">
+                                            <div class="box-item">
+                                                <a data-src="images/folio/products/products-1/5.jpg" class="popup-image" data-sub-html=" repeat predefined">
+                                                <span class="overlay"></span> 
+                                                <img  src="images/folio/products/products-1/thumbs/5.jpg"   alt="">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- 5 end-->                            
+                                    <!-- 6 -->
+                                    <div class="gallery-item">
+                                        <div class="grid-item-holder">
+                                            <div class="box-item">
+                                                <a data-src="images/folio/products/products-1/6.jpg" class="popup-image" data-sub-html="making this">
+                                                <span class="overlay"></span> 
+                                                <img  src="images/folio/products/products-1/thumbs/6.jpg"   alt="">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- 6 end-->
+                                    <!-- 7 -->
+                                    <div class="gallery-item">
+                                        <div class="grid-item-holder">
+                                            <div class="box-item">
+                                                <a data-src="images/folio/products/products-1/7.jpg" class="popup-image" data-sub-html="All the Lorem Ipsum">
+                                                <span class="overlay"></span> 
+                                                <img  src="images/folio/products/products-1/thumbs/7.jpg"   alt="">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- 7 end-->
+                                    <!-- 8 -->
+                                    <div class="gallery-item">
+                                        <div class="grid-item-holder">
+                                            <div class="box-item">
+                                                <a data-src="images/folio/products/products-1/8.jpg" class="popup-image" data-sub-html="repeat predefined chunks as necessary">
+                                                <span class="overlay"></span> 
+                                                <img  src="images/folio/products/products-1/thumbs/8.jpg"   alt="">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- 8 end-->
+                                </div>
+                                <!-- end gallery items -->               
+                                <div class="custom-inner-holder">
+                                    <!-- 1 -->	
+                                    <div class="custom-inner">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <h3>产品细节</h3>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <h4>Lorem Ipsum available</h4>
+                                                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words</p>
+                                                <ul>
+                                                    <li>Door portals plan</li>
+                                                    <li>Furniture specifications</li>
+                                                    <li>Interior design</li>
+                                                </ul>
+                                                <div class="content-nav">
+                                                    <ul>
+                                                        <li><a href="products-4.php" class=" text-link">前 页</a></li>
+                                                        <li><a href="products-2.php" class=" text-link">后 页</a></li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- 1 -->
+                                </div>
+                                <!-- custom-link-holder  end -->
+                            </div>
+                        </section>
+                    </div>
+                    <!-- content end -->
+                    <!-- content footer-->                                     
+                    <?php 
+                        include_once("footer.html");
+                    ?>       
+                    <!-- content footer end -->
+                    <!-- share  -->
+                    <div class="share-inner" id="ckepop">
+                        <div class="share-container  isShare"  data-share="[]">
+                                <i class="fa"><a class="jiathis_button_weixin"></a></i>
+                                <i class="fa"><a class="jiathis_button_qzone"></a></i>
+                                <i class="fa"><a class="jiathis_button_tsina"></a></i>
+                                <i class="fa"><a class="jiathis_button_tqq"></a></i>
+                                <i class="fa"><a class="jiathis_button_renren"></a></i>
+                                <i class="fa"><a class="jiathis jiathis_txt jiathis_separator jtico jtico_jiathis" ></a></i>
+                        </div>
+                        <div class="close-share"></div>
+                    </div>
+                    <!-- share end -->
+                </div>
+                <!-- content-holder  end-->
+            </div>
+            <!-- wrapper end -->
+            <!-- Fixed footer -->
+            <footer class="fixed-footer">
+                <div class="footer-social" id="ckepop">
+                    <ul>
+                        <li><i class="fa"><a class="jiathis_button_weixin"></a></i></li>
+                        <li><i class="fa"><a class="jiathis_button_qzone"></a></i></li>
+                        <li><i class="fa"><a class="jiathis_button_tsina"></a></i></li>
+                        <li><i class="fa"><a class="jiathis_button_tqq"></a></i></li>
+                        <li><i class="fa"><a class="jiathis_button_renren"></a></i></li>
+                        <li><i class="fa"><a class="jiathis jiathis_txt jiathis_separator jtico jtico_jiathis" ></a></i></li>
+                    </ul>
+                </div>
+                <!-- Header  title --> 
+                <div class="footer-title">
+                    <h2><a  href="#"></a></h2>
+                </div>
+                <!-- Header  title  end-->
+            </footer>
+            <!-- Fixed footer end-->
+        </div>
+        <!-- Main end -->
+        <!--=============== scripts  ===============-->
+        <script type="text/javascript" src="http://v3.jiathis.com/code/jia.js?uid=1" charset="utf-8"></script>
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/plugins.js"></script>
+        <script type="text/javascript" src="js/scripts.js"></script>
+        <script>
+            select('nav_bar_products');
+		</script>
+    </body>
+</html>
